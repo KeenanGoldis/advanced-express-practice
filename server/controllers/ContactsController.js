c12edcd8ff9d5215483c8507a3c275983d557941
unknown <keenangoldis@gmail.com>
2017-09-10 18:52:52 -0500

    Steps 1-3 Complete I Think

---
+++ server/controllers/ContactsController.js
@@ -0,0 +1,17 @@
+import contacts from "./contacts"
+
+export function list(request, response) {
+ return response.json([]);
+}
+export function show(request, response) {
+ return response.json({theId: request.params.id});
+}
+export function create(request, response) {
+ return response.json({});
+}
+export function update(request, response) {
+ return response.json({theId: request.params.id});
+}
+export function remove(request, response) {
+ return response.json({});
+}
--- server/index.js
+++ server/index.js
@@ -1,10 +1,43 @@
 import express from "express";
-import comments from "./comments";
-import products from "./products";
-import vehicle from "./vehicles";
-import contacts from "./contacts";
+import bodyParser from "body-parser";
+import ContactRoutes from "./routes/ContactRoutes";
+app.use(contactRoutes);
+
+import mongoose from "mongoose";
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost");
+
 
 const app = express();
+app.use(bodyParser.json());
+app.use(ContactRoutes);
+app.use(VehicleRoutes);
+app.use(CommentRoutes);
+app.use(ProductRoutes);
+
+app.get("/",function(request,response){
+  return response.json({hello:"world"});
+})
+
+// app.get("/contacts",function(request,response){
+//   return response.json({contacts})
+// })
+
+// app.get("/vehicles",function(request,response){
+//   return response.json({vehicles})
+// })
+
+// app.get("/comments",function(request,response){
+//   return response.json({comments})
+// })
+
+// app.get("/products",function(request,response){
+//   return response.json({products})
+// })
+
+app.get("/contacts/:id",function(request,response){
+  return response.json({})
+})
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
