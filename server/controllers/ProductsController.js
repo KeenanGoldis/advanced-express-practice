e6030951930ce2fd9bc00b028719fa1a9d76fc56
unknown <keenangoldis@gmail.com>
2017-09-11 17:35:39 -0500

    finished with part 4 maybe

--- server/controllers/ProductsController.js
+++ server/controllers/ProductsController.js
@@ -1,14 +1,33 @@
 import products from "./products";
 
 export function list(request, response) {
- return response.json([]);
+  ContactModel.find({}).exec()
+  promise.then(products => {
+    return response.json(products);
+  });
 }
+
 export function show(request, response) {
- return response.json({theId: request.params.id});
+  let productId = request.params.id;
+
+  let singleProduct = products.find(function(product){
+    return productId === product._id;
+  })
+ return response.json(singleProduct);
 }
+
 export function create(request, response) {
- return response.json({});
-}
+   const product = new ProductModel({
+     name: request.body.firstName,
+     description: request.body.description
+   });
+   contact.save()
+   .then(product => {
+     return response.json(product);
+   });
+ };
+
+
 export function update(request, response) {
  return response.json({theId: request.params.id});
 }
